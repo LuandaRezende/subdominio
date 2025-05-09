00fc9a731a263456b17ac80379601d9f1db63413
LuandaRezende <luanda.rezende@hotmail.com>
2025-05-09 10:51:01 -0300

    send project

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <CookieReader />
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import CookieReader from './components/CookieReader.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    CookieReader
   }
 }
 </script>
---
+++ src/components/CookieReader.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+      <h2>Leitor de Cookies do Subdomínio</h2>
+      <div>
+        <h3>Cookies Compartilhados:</h3>
+        <pre>{{ cookies }}</pre>
+      </div>
+      <button @click="readCookies">Ler Cookies</button>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        cookies: ''
+      };
+    },
+    methods: {
+      readCookies() {
+        this.cookies = document.cookie;
+      }
+    },
+    mounted() {
+      this.readCookies();
+    }
+  };
+  </script>
+  
+  <style scoped>
+  /* Estilos opcionais */
+  pre {
+    margin-top: 10px;
+    border: 1px solid #ccc;
+    padding: 10px;
+    background-color: #f4f4f4;
+  }
+  button {
+    padding: 8px 15px;
+    cursor: pointer;
+  }
+  </style>
